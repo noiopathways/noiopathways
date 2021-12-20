de45856c07ff78abe39937242635ae3adde726be
puakehaulani <puakehaulani@gmail.com>
2021-12-20 15:50:50 -0800

    fixed footer to bottom, added content

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import { FaHandHoldingHeart, FaRegCopyright } from 'react-icons/fa';
+
+import './style.css'
 
 function Footer() {
     return (
-        <>
-            <h1>FOOTER</h1>
-        </>
+        <Container className="footer">
+            made with <FaHandHoldingHeart /> by <a href="https://www.linkedin.com/in/lexijack/" target="_blank" rel="noopener noreferrer">lexi jack</a><br />
+            <FaRegCopyright /> 2021
+        </Container>
     )
 }
 
