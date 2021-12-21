2ec7aefffdb3438c4cc47ea75dbb7341a7d3563a
puakehaulani <puakehaulani@gmail.com>
2021-12-20 21:17:41 -0800

    founders structure and dummy text

--- src/Components/Founder/index.js
+++ src/Components/Founder/index.js
@@ -7,13 +7,18 @@ import './style.css'
 function Founder() {
     return (
         <Container className="founder">
-            <h1 className="text-center">About Our Founder</h1>
             <Row xs={1} sm={2}>
                 <Col className="text-center" style={{ backgroundColor: "bisque" }}>
                     Col1
                 </Col>
                 <Col className="text-center" style={{ backgroundColor: "wheat" }}>
-                    Col2</Col>
+                    <h1 className="text-center">About Our Founder</h1>
+                    Waistcoat you probably haven't heard of them hoodie, cold-pressed jianbing tattooed copper mug craft beer pabst gentrify. XOXO before they sold out craft beer hexagon butcher poutine pickled DIY fashion axe selfies. Deep v viral semiotics echo park +1, blue bottle hammock cred food truck everyday carry. Irony poutine raclette craft beer.
+
+                    Four loko blue bottle craft beer kitsch enamel pin prism four dollar toast. Banh mi small batch bespoke shoreditch, narwhal mustache tacos seitan listicle affogato kogi kinfolk vaporware polaroid portland. Before they sold out small batch taxidermy, succulents pabst taiyaki church-key single-origin coffee affogato 90's everyday carry sriracha. Deep v kitsch kombucha small batch, blog knausgaard iPhone etsy selfies master cleanse live-edge banjo poke. Health goth cray before they sold out mustache XOXO. Pug mumblecore adaptogen hella, readymade freegan cardigan hashtag subway tile iPhone 90's twee enamel pin wolf woke.
+
+                    Dummy text? More like dummy thicc text, amirite?
+                </Col>
             </Row>
         </Container>
     )
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero'
 import About from '../Components/About'
 import Values from '../Components/Values'
 import Services from '../Components/Services'
+import Founder from '../Components/Founder'
 import Footer from '../Components/Footer'
 
 function Landing() {
@@ -13,6 +14,7 @@ function Landing() {
             <About />
             <Values />
             <Services />
+            <Founder />
             <Footer />
         </>
     )
