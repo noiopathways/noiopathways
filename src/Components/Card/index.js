1d9c81d21df191d79e498be95eaf974aba240b1c
puakehaulani <puakehaulani@gmail.com>
2021-12-20 18:13:23 -0800

    centered title

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -8,7 +8,7 @@ function CardComponent(props) {
         <Card>
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
+                <Card.Title className="text-center">{props.title}</Card.Title>
                 <Card.Text>
                     {props.text}
                 </Card.Text>
