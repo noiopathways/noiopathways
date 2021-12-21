a738f4fb5d583ee6672319a73eef7b36e12b56bc
puakehaulani <puakehaulani@gmail.com>
2021-12-20 18:17:43 -0800

    updated container to div to fix styling

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
 import { FaHandHoldingHeart, FaRegCopyright } from 'react-icons/fa';
 
 import './style.css'
 
 function Footer() {
     return (
-        <Container className="footer">
+        <div className="footer">
             made with <FaHandHoldingHeart /> by <a href="https://www.linkedin.com/in/lexijack/" target="_blank" rel="noopener noreferrer">lexi jack</a><br />
             <FaRegCopyright /> 2021
-        </Container>
+        </div>
     )
 }
 
