c735cf1aabf76d34037ff6fa2342bee655338766
puakehaulani <puakehaulani@gmail.com>
2021-12-21 00:03:43 -0800

    removed container

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
@@ -8,19 +7,17 @@ function Navigation() {
     return (
         <Navbar bg="light" expand="md" sticky="top">
             {/* todo: revisit sticky vs fixed and browser support */}
-            <Container>
-                <Navbar.Brand href="#">Noio Pathways</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                    <Nav>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#values">Values</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#founder">Founder</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+            <Navbar.Brand href="#">Noio Pathways</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#values">Values</Nav.Link>
+                    <Nav.Link href="#services">Services</Nav.Link>
+                    <Nav.Link href="#founder">Founder</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
