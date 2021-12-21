63064dfcf56859ae690c4a48fd874844b04b4fa4
puakehaulani <puakehaulani@gmail.com>
2021-12-20 18:20:41 -0800

    services placeholder

---
+++ src/Components/Services/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+import CardGrid from '../CardGrid'
+import './style.css'
+
+function Services() {
+    const servicesObject = [
+        {
+            title: "Service 1",
+            image: "https://picsum.photos/200",
+            text: "Umami you probably haven't heard of them health goth, aesthetic vegan beard palo santo bushwick."
+        },
+        {
+            title: "Service 2",
+            image: "https://picsum.photos/200",
+            text: "I'm baby salvia waistcoat fashion axe small batch. Craft beer godard ugh hella slow-carb occupy chillwave coloring book flexitarian 90's farm-to-table photo booth."
+        },
+        {
+            title: "Service 3",
+            image: "https://picsum.photos/200",
+            text: " Cliche occupy humblebrag, swag actually VHS vape normcore bespoke keffiyeh hammock trust fund paleo."
+        }
+    ]
+    return (
+        <Container className="services">
+            <h1 className="text-center">Our Services</h1>
+            <CardGrid data={servicesObject} />
+        </Container>
+    )
+}
+
+export default Services
