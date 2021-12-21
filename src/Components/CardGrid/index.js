6ba1d25304481e3a21c11924f95f34c12c4956ac
puakehaulani <puakehaulani@gmail.com>
2021-12-20 18:09:41 -0800

    card data obj passed in props

--- src/Components/CardGrid/index.js
+++ src/Components/CardGrid/index.js
@@ -6,23 +6,20 @@ import Col from 'react-bootstrap/Col'
 import CardComponent from '../Card'
 import './style.css'
 
-function CardGrid() {
+function CardGrid(props) {
+    const colContent = props.data.map(i => (
+        <Col key={i.title}>
+            <CardComponent image={i.image} title={i.title} text={i.text} />
+        </Col>)
+    )
+
     return (
         <Container>
             <Row xs={1} sm={3}>
-                <Col>
-                    <CardComponent image="http://placekitten.com/g/200/200" title="First" text="I'm baby williamsburg id mlkshk kitsch laborum. Woke meditation cornhole hammock, kale chips try-hard distillery shabby chic flexitarian man braid austin 3 wolf moon XOXO. " />
-                </Col>
-                <Col>
-                    <CardComponent image="http://placekitten.com/g/200/200" title="Second" text="Ut proident taxidermy, celiac lorem pitchfork banh mi kale chips viral excepteur crucifix ennui. Tofu sustainable portland cupidatat consectetur lomo, occaecat selvage try-hard readymade woke small batch." />
-                </Col>
-                <Col>
-                    <CardComponent image="http://placekitten.com/g/200/200" title="Third" text="Cloud bread ex stumptown poke next level dolore pabst you probably haven't heard of them williamsburg pop-up adipisicing wolf chillwave bicycle rights." />
-                </Col>
+                {colContent}
             </Row>
         </Container>
     )
-
 }
 
 export default CardGrid
