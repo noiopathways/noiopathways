9a5269df0abd93c57fc283dba2e7c076e122392e
puakehaulani <puakehaulani@gmail.com>
2021-12-20 18:51:29 -0800

    structure started

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import './style.css'
 
 function About() {
     return (
         <Container className="about">
-            About
+            <h1 className="text-center">About Noio Pathways</h1>
+            <Row xs={1} sm={2}>
+                <Col className="text-center" style={{ backgroundColor: "wheat" }}>
+                    Col1
+                </Col>
+                <Col className="text-center" style={{ backgroundColor: "bisque" }}>
+                    Col2</Col>
+            </Row>
         </Container>
     )
 }
