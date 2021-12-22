ecd2ad9ee2f1756dce6a80d9926002845a38f5b8
puakehaulani <puakehaulani@gmail.com>
2021-12-22 00:45:22 -0800

    added google font

--- src/Components/Values/index.js
+++ src/Components/Values/index.js
@@ -7,17 +7,17 @@ function Values() {
     const valuesObject = [
         {
             title: "Value 1",
-            image: "http://placekitten.com/g/200/200",
+            image: "http://placekitten.com/200/200",
             // text: "I'm baby williamsburg id mlkshk kitsch laborum. Woke meditation cornhole hammock, kale chips try-hard distillery shabby chic flexitarian man braid austin 3 wolf moon XOXO."
         },
         {
             title: "Value 2",
-            image: "http://placekitten.com/g/200/200",
+            image: "http://placekitten.com/200/200",
             // text: "Ut proident taxidermy, celiac lorem pitchfork banh mi kale chips viral excepteur crucifix ennui. Tofu sustainable portland cupidatat consectetur lomo, occaecat selvage try-hard readymade woke small batch."
         },
         {
             title: "Value 3",
-            image: "http://placekitten.com/g/200/200",
+            image: "http://placekitten.com/200/200",
             // text: "Cloud bread ex stumptown poke next level dolore pabst you probably haven't heard of them williamsburg pop-up adipisicing wolf chillwave bicycle rights."
         }
     ]
