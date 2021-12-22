f8c989e6d7802412c11dbcc6d58ac074078eb77c
puakehaulani <puakehaulani@gmail.com>
2021-12-21 19:37:12 -0800

    todo note

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Form() {
     return (
+        // TODO: update email below to jamies
         <form action="https://formsubmit.co/puakehaulani.dev@gmail.com" method="POST">
             <input type="text" name="name" placeholder="Your Name" required />
             <input type="email" name="email" placeholder="Your Email" required />
