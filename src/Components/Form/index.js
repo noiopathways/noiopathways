7c3e820e1da81d4ddcd4471fbc00a6d7e623adc5
puakehaulani <puakehaulani@gmail.com>
2021-12-21 20:12:30 -0800

    added container, rows and cols to form

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import './style.css'
 
 function Form() {
     return (
-        // TODO: update email below to jamies
-        <form action="https://formsubmit.co/puakehaulani.dev@gmail.com" method="POST">
-            {/* TODO: update redirect page link below */}
-            <input type="hidden" name="_next" value="http://localhost:3000/"></input>
-            <input type="hidden" name="_subject" value="New Website Contact Form Message"></input>
-            <input type="text" name="name" placeholder="Your Name" required />
-            <input type="email" name="email" placeholder="Your Email" required />
-            <input type="text" name="message" placeholder="Message" required />
-            <button type="submit">Submit</button>
-        </form>
+        <Container className="contactForm">
+            {/* TODO: update email below to jamies */}
+            < form action="https://formsubmit.co/puakehaulani.dev@gmail.com" method="POST" >
+                {/* TODO: update redirect page link below */}
+                < input type="hidden" name="_next" value="http://localhost:3000/" ></input >
+                <input type="hidden" name="_subject" value="New Website Contact Form Message"></input>
+                <Row>
+                    <Col><input type="text" name="name" placeholder="Your Name" required /></Col>
+                    <Col><input type="email" name="email" placeholder="Your Email" required /></Col>
+                </Row>
+                <Row>
+                    <input type="text" name="message" placeholder="Message" required />
+                </Row>
+                <Row>
+                    <button type="submit">Submit</button>
+                </Row>
+            </form >
+        </Container>
     )
 }
 
