bd54921c8060772d76630566acd7e6219c421447
puakehaulani <puakehaulani@gmail.com>
2021-12-21 19:58:45 -0800

    added redirect and subject

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -4,6 +4,9 @@ function Form() {
     return (
         // TODO: update email below to jamies
         <form action="https://formsubmit.co/puakehaulani.dev@gmail.com" method="POST">
+            {/* TODO: update redirect page link below */}
+            <input type="hidden" name="_next" value="http://localhost:3000/"></input>
+            <input type="hidden" name="_subject" value="New Website Contact Form Message"></input>
             <input type="text" name="name" placeholder="Your Name" required />
             <input type="email" name="email" placeholder="Your Email" required />
             <input type="text" name="message" placeholder="Message" required />
