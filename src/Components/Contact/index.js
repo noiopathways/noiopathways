1d3540c412846f62e7c294cf6b7654e627628dfe
puakehaulani <puakehaulani@gmail.com>
2021-12-21 23:34:09 -0800

    styling fixes for contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -11,7 +11,7 @@ function Contact() {
     return (
         <Container className="contact" id="contact">
             <Row xs={1} sm={2} style={{ display: "flex", justifyContent: "center" }}>
-                <Col sm={4} style={{ backgroundColor: "bisque" }}>
+                <Col sm={4}>
                     <h1 className="text-center">Contact Us</h1>
                     <Container>Mollit enim fam af nulla hexagon narwhal pug bicycle rights deep v veniam.<br />
                         <div><FaLinkedin /> <FaInstagramSquare /></div></Container>
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -3,8 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
-
-import './style.css'
+import Button from 'react-bootstrap/Button'
 
 function FormComponent() {
     return (
@@ -15,18 +14,18 @@ function FormComponent() {
                 < input type="hidden" name="_next" value="http://localhost:3000/" />
                 <input type="hidden" name="_template" value="box" />
                 <input type="hidden" name="_subject" value="New Website Contact Form Message" />
-                <Row>
-                    <Col><Form.Control type="text" name="name" placeholder="Your Name" /></Col>
-                    <Col><Form.Control type="email" name="email" placeholder="Your Email" /></Col>
+                <Row style={{ paddingBottom: 10 }}>
+                    <Col sm={6} style={{ paddingRight: 5 }}><Form.Control type="text" name="name" placeholder="Your Name" /></Col>
+                    <Col sm={6} style={{ paddingLeft: 5 }}><Form.Control type="email" name="email" placeholder="Your Email" /></Col>
                 </Row>
-                <Row>
-                    <Form.Control as="textarea" name="message" placeholder="Message" required />
+                <Row style={{ paddingBottom: 10 }}>
+                    <Col><Form.Control as="textarea" name="message" placeholder="Message" required /></Col>
                 </Row>
-                <Row>
-                    <button type="submit">Submit</button>
+                <Row >
+                    <Col style={{ display: "flex", justifyContent: "end" }}><Button type="submit">Submit</Button></Col>
                 </Row>
             </Form >
-        </Container>
+        </Container >
     )
 }
 
