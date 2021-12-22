47b6bc8f1589248323f5a18bdcf44adad7b0ff2d
puakehaulani <puakehaulani@gmail.com>
2021-12-21 20:52:27 -0800

    updated founder image and text

--- src/Components/Founder/index.js
+++ src/Components/Founder/index.js
@@ -10,11 +10,11 @@ function Founder() {
         <Container className="founder" id="founder">
             <Row xs={1} sm={2}>
                 <Col className="image-container">
-                    <Image src="https://generative-placeholders.glitch.me/image?width=350&height=400&style=joy-division" />
+                    <Image src="./images/founder.png" height={400} width={400} fluid />
                 </Col>
                 <Col className="text-center my-auto">
                     <h1 className="text-center">About Our Founder</h1>
-                    Waistcoat you probably haven't heard of them hoodie, cold-pressed jianbing tattooed copper mug craft beer pabst gentrify. XOXO before they sold out craft beer hexagon butcher poutine pickled DIY fashion axe selfies. Deep v viral semiotics echo park +1, blue bottle hammock cred food truck everyday carry. Irony poutine raclette craft beer.
+                    Jamie is a hammah even tho she Punahou, cold-pressed jianbing tattooed copper mug craft beer pabst gentrify. XOXO before they sold out craft beer hexagon butcher poutine pickled DIY fashion axe selfies. Deep v viral semiotics echo park +1, blue bottle hammock cred food truck everyday carry. Irony poutine raclette craft beer.
 
                     Four loko blue bottle craft beer kitsch enamel pin prism four dollar toast. Banh mi small batch bespoke shoreditch, narwhal mustache tacos seitan listicle affogato kogi kinfolk vaporware polaroid portland. Before they sold out small batch taxidermy, succulents pabst taiyaki church-key single-origin coffee affogato 90's everyday carry sriracha. Deep v kitsch kombucha small batch, blog knausgaard iPhone etsy selfies master cleanse live-edge banjo poke. Health goth cray before they sold out mustache XOXO. Pug mumblecore adaptogen hella, readymade freegan cardigan hashtag subway tile iPhone 90's twee enamel pin wolf woke.
 
