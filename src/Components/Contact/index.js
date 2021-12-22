1806bb4a6cb7baed5308941da510455ccc30eb2e
puakehaulani <puakehaulani@gmail.com>
2021-12-21 19:59:05 -0800

    added dummy info to first col, styling

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from "react-bootstrap/Row"
 import Col from 'react-bootstrap/Col'
+import { FaLinkedin, FaInstagramSquare } from "react-icons/fa";
 
 import Form from '../Form'
+import './style.css'
 
 function Contact() {
     return (
-        <Container>
-            <Row xs={1} sm={2}>
-                <Col>
-                    Col1
+        <Container className="contact" id="contact">
+            <Row xs={1} sm={2} style={{ display: "flex", justifyContent: "center" }}>
+                <Col sm={4} style={{ backgroundColor: "bisque" }}>
+                    <h1 className="text-center">Contact Us</h1>
+                    Mollit enim fam af nulla hexagon narwhal pug bicycle rights deep v veniam.<br />
+                    <div><FaLinkedin /> <FaInstagramSquare /></div>
                 </Col>
-                <Col>
+                <Col sm={8} style={{ backgroundColor: "wheat" }}>
                     <Form />
                 </Col>
             </Row>
