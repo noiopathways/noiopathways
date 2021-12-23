94a07982e3d9151eb1fa9101812bccccbed67df9
puakehaulani <puakehaulani@gmail.com>
2021-12-22 21:11:10 -0800

    logo added to hero

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 import './style.css'
 
 function Hero() {
     return (
         <div className="hero" id="home">
-
+            <Container>
+                <Row><Image className="logoText" src="./images/logoText.png" fluid /></Row>
+                <Row><span>Tagline here</span></Row>
+            </Container>
         </div>
     )
 }
