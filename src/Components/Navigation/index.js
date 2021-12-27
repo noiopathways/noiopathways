95465f37bc6effed82274364f6c1ed521e4ddaf5
puakehaulani <puakehaulani@gmail.com>
2021-12-26 21:06:53 -0800

    logic for displaying navbar/logo from pageYOffset

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Image from 'react-bootstrap/Image'
@@ -6,10 +6,21 @@ import Image from 'react-bootstrap/Image'
 import './style.css'
 
 function Navigation() {
+    const [transparentBg, setTransparentBg] = useState(true)
+    useEffect(() => {
+        window.onscroll = () => {
+            if (window.pageYOffset > 350) {
+                setTransparentBg(false)
+            } else {
+                setTransparentBg(true)
+            }
+        }
+    }, []);
+
     return (
-        <Navbar className="navBackgroundColor" variant="dark" expand="md" sticky="top">
+        <Navbar className={` ${transparentBg ? "navBackgroundTransparent" : "navBackgroundColor"}`} variant="dark" expand="md" sticky="top">
             {/* todo: revisit sticky vs fixed and browser support */}
-            <Navbar.Brand href="#"><Image src="./images/logo.png" height={50} /></Navbar.Brand>
+            <Navbar.Brand href="#"><Image src={` ${transparentBg ? "" : "./images/logo.png"}`} height={50} /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav>
