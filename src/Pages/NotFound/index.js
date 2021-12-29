4a1497be893892385e5506923d84c7f017de52cd
puakehaulani <puakehaulani@gmail.com>
2021-12-28 19:54:10 -0800

    added return button and styling, functioning

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,17 +1,32 @@
 import React from "react"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom';
 
 import './style.css'
 
+const btnStyle = {
+    color: "#10404c",
+    borderColor: "#10404c",
+    backgroundColor: "transparent",
+    padding: "5px 40px"
+}
+
 function NotFound() {
     return (
-        <div className="notFound">
-            <Image src="./images/404.png" fluid />
-
-            {/* <Button>go</Button> */}
-
-        </div>
+        <Container fluid className="notFound">
+            <Row>
+                <Image src="./images/404.png" fluid />
+            </Row>
+            <Row className="text-center">
+                <Col>
+                    <Link to="/"><Button style={btnStyle}>return</Button></Link>
+                </Col>
+            </Row>
+        </Container>
 
 
     )
