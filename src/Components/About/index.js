7e483f24452f60c20762ac95fa6ed224d5416e2a
puakehaulani <puakehaulani@gmail.com>
2021-12-29 13:22:54 -0800

    banner styling spacing adjustments across comps

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -10,14 +10,15 @@ function About() {
     return (
         <Container className="about" id="about">
             <Row xs={1} sm={2}>
+                <Col className="image-container">
+                    <Image src="./images/about.jpg" height={400} width={400} rounded fluid />
+                </Col>
                 <Col className="text-center my-auto textContainerAbout">
                     <h1 className="text-center">About Noio Pathways</h1>
 
                     Ugh williamsburg unicorn keytar labore meggings fam hashtag ut velit. Tbh dolore live-edge, selfies YOLO shoreditch eu subway tile est kombucha. Cronut readymade irony blog. Dolore mlkshk schlitz pok pok gluten-free skateboard hella lumbersexual heirloom, cupidatat gastropub dolore disrupt adipisicing. Vape pug vegan neutra ullamco officia pariatur polaroid semiotics keytar vice affogato knausgaard. Art party laborum offal in ut dolore mlkshk literally pour-over chambray pariatur. Keffiyeh heirloom locavore, aliqua dreamcatcher in pitchfork blog hashtag umami.
                 </Col>
-                <Col className="image-container">
-                    <Image src="./images/about.jpg" height={400} width={400} rounded fluid />
-                </Col>
+
             </Row>
         </Container>
     )
--- src/Components/Approach/index.js
+++ src/Components/Approach/index.js
@@ -12,9 +12,6 @@ function Approach() {
         <Container className="approach" id="approach">
             <Banner />
             <Row xs={1} sm={2}>
-                <Col className="image-container">
-                    <Image src="./images/approach.jpg" height={400} width={400} rounded fluid />
-                </Col>
                 <Col className="text-center my-auto textContainerApproach">
                     <h1 className="text-center">Our Approach</h1>
                     Jamie is a hammah even tho she Punahou, cold-pressed jianbing tattooed copper mug craft beer pabst gentrify. XOXO before they sold out craft beer hexagon butcher poutine pickled DIY fashion axe selfies. Deep v viral semiotics echo park +1, blue bottle hammock cred food truck everyday carry. Irony poutine raclette craft beer.
@@ -22,6 +19,9 @@ function Approach() {
                     Four loko blue bottle craft beer kitsch enamel pin prism four dollar toast. Banh mi small batch bespoke shoreditch, narwhal mustache tacos seitan listicle affogato kogi kinfolk vaporware polaroid portland. Before they sold out small batch taxidermy, succulents pabst taiyaki church-key single-origin coffee affogato 90's everyday carry sriracha. Deep v kitsch kombucha small batch, blog knausgaard iPhone etsy selfies master cleanse live-edge banjo poke. Health goth cray before they sold out mustache XOXO. Pug mumblecore adaptogen hella, readymade freegan cardigan hashtag subway tile iPhone 90's twee enamel pin wolf woke.
 
                     Dummy text? More like dummy thicc text, amirite?
+                </Col>
+                <Col className="image-container">
+                    <Image src="./images/approach.jpg" height={400} width={400} rounded fluid />
                 </Col>
             </Row>
         </Container>
