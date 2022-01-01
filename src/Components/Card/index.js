b240687344247483d3ffa63b2528293227dd234e
puakehaulani <puakehaulani@gmail.com>
2022-01-01 14:06:29 -0800

    client provided quote content

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -9,7 +9,7 @@ function CardComponent(props) {
             <Card.Img variant="top" src={props.image} style={{ borderRadius: 10 }} />
             <Card.Body>
                 <Card.Title as="h3" className="text-center">{props.title}</Card.Title>
-                <Card.Text>
+                <Card.Text className="text-center">
                     {props.text}
                 </Card.Text>
             </Card.Body>
--- src/Components/Quotes/index.js
+++ src/Components/Quotes/index.js
@@ -7,19 +7,19 @@ import './style.css'
 function Quotes() {
     const quotesObject = [
         {
-            title: "name 1",
+            title: "Dinah Wilson",
             image: "./images/quote1.png",
-            // text: "I'm baby williamsburg id mlkshk kitsch laborum. Woke meditation rnhole hammock, kale chips try-hard distillery shabby chic flexitarian man braid austin 3 wolf moon XOXO."
+            text: "Founder & Facilitator of Kent Cultural Diversity Initiative Group (KC-DIG)"
         },
         {
-            title: "name 2",
+            title: "Lara Whitely Binder",
             image: "./images/quote2.png",
-            // text: "Ut proident taxidermy, celiac lorem pitchfork banh mi kale chips viral excepteur crucifix ennui. Tofu sustainable portland cupidatat consectetur lomo, occaecat selvage try-hard readymade woke small batch."
+            text: "Local government climate preparedness professional"
         },
         {
-            title: "name 3",
-            image: "./images/quote1.png",
-            // text: "Cloud bread ex stumptown poke next level dolore pabst you probably haven't heard of them williamsburg pop-up adipisicing wolf chillwave bicycle rights."
+            title: "Kim Yu, MPH",
+            image: "./images/quote3.png",
+            text: "Health Equity Consultant"
         }
     ]
     return (
