11070b4308c70a0d42c15bca124f4b6118a183d8
puakehaulani <puakehaulani@gmail.com>
2022-01-01 16:47:25 -0800

    client content finalized

--- src/Components/Approach/index.js
+++ src/Components/Approach/index.js
@@ -14,17 +14,22 @@ function Approach() {
             <Row xs={1} sm={2}>
                 <Col className="text-center my-auto textContainerApproach">
                     <h1 className="text-center">Our Approach</h1>
-                    Jamie is a hammah even tho she Punahou, cold-pressed jianbing tattooed copper mug craft beer pabst gentrify. XOXO before they sold out craft beer hexagon butcher poutine pickled DIY fashion axe selfies. Deep v viral semiotics echo park +1, blue bottle hammock cred food truck everyday carry. Irony poutine raclette craft beer.
-
-                    Four loko blue bottle craft beer kitsch enamel pin prism four dollar toast. Banh mi small batch bespoke shoreditch, narwhal mustache tacos seitan listicle affogato kogi kinfolk vaporware polaroid portland. Before they sold out small batch taxidermy, succulents pabst taiyaki church-key single-origin coffee affogato 90's everyday carry sriracha. Deep v kitsch kombucha small batch, blog knausgaard iPhone etsy selfies master cleanse live-edge banjo poke. Health goth cray before they sold out mustache XOXO. Pug mumblecore adaptogen hella, readymade freegan cardigan hashtag subway tile iPhone 90's twee enamel pin wolf woke.
-
-                    Dummy text? More like dummy thicc text, amirite?
+                    <p>
+                        Our team is committed to supporting work that centers equity, collaboration, and partnership. Our work supports clients in identifying pathways to equitable solutions, by building capacity and leadership, and developing community-centered programs/policies. Noio Pathways works with clients across an array of issue areas, from public health to urban planning, and sectors, from local government and academia, to BIPOC-led nonprofits and small businesses. We choose to work with individuals, organizations, and communities that are dedicated to equitable processes, a learning mindset, and constructive dialogues, and are looking to dig into how to make the world a better place. We recognize that addressing systemic racism and guiding clients around how to best integrate and address root causes requires a wide range of tools and approaches, as well as an understanding that it takes time and commitment to create lasting change. We work to create a baseline for shared understanding, and utilize a wide array of tools and frameworks such as power mapping analyses, the spectrum of community engagement to ownership, and interactive coaching.
+                    </p>
                 </Col>
                 <Col className="image-container">
                     <Image src="./images/approach.jpg" height={400} width={400} rounded fluid />
                 </Col>
             </Row>
-        </Container>
+            <div className="approachList">We strive to:
+                <ul>
+                    <li>Center and uplift the leadership of those most impacted, by continually asking who is in the room, who is missing from the room, and who is making decisions.</li>
+                    <li>Address the root causes of racism and inequity with systemic changes to organizational practices, policies, and processes, and removal of barriers to access and resources.</li>
+                    <li>Build a thorough understanding of existing inequities as well as the systemic and historical structures that contribute to those ongoing inequities.</li>
+                </ul>
+            </div>
+        </Container >
     )
 }
 
