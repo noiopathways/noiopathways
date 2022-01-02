b182a27e54fe6322a44bf23079fe4ead754a0cf9
puakehaulani <puakehaulani@gmail.com>
2022-01-01 18:03:29 -0800

    client update

--- src/Components/Approach/index.js
+++ src/Components/Approach/index.js
@@ -15,20 +15,13 @@ function Approach() {
                 <Col className="text-center my-auto textContainerApproach">
                     <h1 className="text-center">Our Approach</h1>
                     <p>
-                        Our team is committed to supporting work that centers equity, collaboration, and partnership. Our work supports clients in identifying pathways to equitable solutions, by building capacity and leadership, and developing community-centered programs/policies. Noio Pathways works with clients across an array of issue areas, from public health to urban planning, and sectors, from local government and academia, to BIPOC-led nonprofits and small businesses. We choose to work with individuals, organizations, and communities that are dedicated to equitable processes, a learning mindset, and constructive dialogues, and are looking to dig into how to make the world a better place. We recognize that addressing systemic racism and guiding clients around how to best integrate and address root causes requires a wide range of tools and approaches, as well as an understanding that it takes time and commitment to create lasting change. We work to create a baseline for shared understanding, and utilize a wide array of tools and frameworks such as power mapping analyses, the spectrum of community engagement to ownership, and interactive coaching.
+                        Our team is committed to supporting work that centers equity, collaboration, and partnership. Our work supports clients in identifying pathways to equitable solutions, by building capacity and leadership, and developing community-centered programs/policies. Noio Pathways works with clients across an array of issue areas, from public health to urban planning, and sectors, from local government and academia, to BIPOC-led nonprofits and small businesses. We choose to work with individuals, organizations, and communities that are dedicated to equitable processes, a learning mindset, and constructive dialogues, and are looking to dig into how to make the world a better place. We recognize that addressing systemic racism and guiding clients around how to address root causes requires a wide range of tools and approaches, as well as an understanding that it takes time and commitment to create lasting organizational and institutional change. We work to create a baseline for shared understanding, and utilize a wide array of tools and frameworks such as facilitative dialogue, power mapping analyses, the spectrum of community engagement to ownership, and coaching, all while centering and uplifting the leadership and needs of those most impacted.
                     </p>
                 </Col>
                 <Col className="image-container">
                     <Image src="./images/approach.jpg" height={400} width={400} rounded fluid />
                 </Col>
             </Row>
-            <div className="approachList">We strive to:
-                <ul>
-                    <li>Center and uplift the leadership of those most impacted, by continually asking who is in the room, who is missing from the room, and who is making decisions.</li>
-                    <li>Address the root causes of racism and inequity with systemic changes to organizational practices, policies, and processes, and removal of barriers to access and resources.</li>
-                    <li>Build a thorough understanding of existing inequities as well as the systemic and historical structures that contribute to those ongoing inequities.</li>
-                </ul>
-            </div>
         </Container >
     )
 }
