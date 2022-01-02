1e0f5975ddd067950da089c83a8a86193701fbcb
puakehaulani <puakehaulani@gmail.com>
2022-01-01 16:04:54 -0800

    client finalized content

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -11,12 +11,13 @@ function Contact() {
     return (
         <Container className="contact" id="contact">
             <Row xs={1} sm={2} style={{ display: "flex", justifyContent: "center", alignItems: 'center' }}>
-                <Col sm={4} className="text-center">
-                    <h1 >Contact Us</h1>
+                <Col sm={4}>
+                    <h1 className="text-center">Contact Us</h1>
                     <Container style={{ paddingBottom: "30px" }}>
-                        We choose to work with individuals, organizations, and communities that are dedicated to equitable processes and change, collaboration, and long-term relationship building. Please reach out to us to learn more about working with Noio Pathways!
-                        <br />
-                        <h1 ><a style={{ color: "#0e404b" }} href="https://www.linkedin.com/in/jstroble/" target="_blank" rel='noreferrer'><FaLinkedin /></a>
+                        <p className="text-center"> Please reach out to us to learn more about working with Noio Pathways!
+                        </p>
+
+                        <h1 className="text-center"><a style={{ color: "#0e404b" }} href="https://www.linkedin.com/in/jstroble/" target="_blank" rel='noreferrer'><FaLinkedin /></a>
                             <a style={{ color: "#0e404b" }} href="https://www.instagram.com/noiopathways/" target="_blank" rel='noreferrer'><FaInstagramSquare /></a></h1>
                     </Container>
                 </Col>
